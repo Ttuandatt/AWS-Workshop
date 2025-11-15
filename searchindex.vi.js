22ca4ed639120caf42e6455853e6caec41641c15
adminmoi <pcanhtuandat@gmail.com>
2025-11-15 15:18:51 +0700

    Deploy update

---
+++ searchindex.vi.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Báo Cáo Thực Tập \u003e Nhật ký công việc",
+    "content": "Mục tiêu tuần 1\rTìm hiểu các quy định Kết nối với các thành viên FCJ khác Hiểu và thực hành các dịch vụ AWS cơ bản Các công việc cần thực hiện trong tuần\rNgày Công việc Ngày bắt đầu Ngày hoàn thành Tài liệu tham khảo 1 - Làm quen với các thành viên FCJ 09/09/2025 09/09/2025 - Đọc và ghi chú các quy định của đơn vị thực tập - Học và thực hành tạo website tĩnh bằng Hugo - Tạo tài khoản AWS. Thực hành tạo Nhóm người dùng và Người dùng (IAM user) 2 - Học và thực hành tạo Ngân sách (Mẫu \u0026 Tùy chỉnh: Chi phí, Sử dụng, Gói tiết kiệm, Đặt chỗ) 10/09/2025 10/09/2025 - Cập nhật nhật ký công việc 3 - Tìm hiểu về các gói hỗ trợ, các loại yêu cầu hỗ trợ, cách tạo yêu cầu hỗ trợ 11/09/2025 11/09/2025 4 - Dịch một bài blog 12/09/2025 12/09/2025 - Cập nhật nhật ký công việc (UI, blog đã dịch) 5 - Học lý thuyết về VPC (Subnet, Route Table, Internet Gateway, NAT Gateway) 13/09/2025 13/09/2025 - Cập nhật nhật ký công việc 6 - Dịch bài blog thứ 2 14/09/2025 14/09/2025 - Cập nhật nhật ký công việc",
+    "description": "Mục tiêu tuần 1\rTìm hiểu các quy định Kết nối với các thành viên FCJ khác Hiểu và thực hành các dịch vụ AWS cơ bản Các công việc cần thực hiện trong tuần\rNgày Công việc Ngày bắt đầu Ngày hoàn thành Tài liệu tham khảo 1 - Làm quen với các thành viên FCJ 09/09/2025 09/09/2025 - Đọc và ghi chú các quy định của đơn vị thực tập - Học và thực hành tạo website tĩnh bằng Hugo - Tạo tài khoản AWS. Thực hành tạo Nhóm người dùng và Người dùng (IAM user) 2 - Học và thực hành tạo Ngân sách (Mẫu \u0026 Tùy chỉnh: Chi phí, Sử dụng, Gói tiết kiệm, Đặt chỗ) 10/09/2025 10/09/2025 - Cập nhật nhật ký công việc 3 - Tìm hiểu về các gói hỗ trợ, các loại yêu cầu hỗ trợ, cách tạo yêu cầu hỗ trợ 11/09/2025 11/09/2025 4 - Dịch một bài blog 12/09/2025 12/09/2025 - Cập nhật nhật ký công việc (UI, blog đã dịch) 5 - Học lý thuyết về VPC (Subnet, Route Table, Internet Gateway, NAT Gateway) 13/09/2025 13/09/2025 - Cập nhật nhật ký công việc 6 - Dịch bài blog thứ 2 14/09/2025 14/09/2025 - Cập nhật nhật ký công việc",
+    "tags": [],
+    "title": "Nhật ký công việc Tuần 1",
+    "uri": "/vi/1-worklog/1.1-week_1/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "BÁO CÁO THỰC TẬP\rThông tin sinh viên:\rHọ và tên: Phan Cảnh Tuấn Đạt Số điện thoại: 0867829257\nEmail: pcanhtuandat@gmail.com\nTrường: Đại học Sài Gòn\nNgành: Công nghệ Thông tin\nLớp: DCT1226\nCông ty thực tập: Amazon Web Services Vietnam Co., Ltd.\nVị trí thực tập: Thực tập sinh FCJ Cloud\nThời gian thực tập: Từ 12/08/2025 đến 12/11/2025\nNội dung báo cáo\r1. Nhật ký công việc\n2. Đề xuất\n3. Bài viết dịch\n4. Sự kiện đã tham gia\n5. Hội thảo\n6. Tự đánh giá\n7. Chia sẻ và phản hồi",
+    "description": "BÁO CÁO THỰC TẬP\rThông tin sinh viên:\rHọ và tên: Phan Cảnh Tuấn Đạt Số điện thoại: 0867829257\nEmail: pcanhtuandat@gmail.com\nTrường: Đại học Sài Gòn\nNgành: Công nghệ Thông tin\nLớp: DCT1226\nCông ty thực tập: Amazon Web Services Vietnam Co., Ltd.\nVị trí thực tập: Thực tập sinh FCJ Cloud\nThời gian thực tập: Từ 12/08/2025 đến 12/11/2025",
+    "tags": [],
+    "title": "Báo Cáo Thực Tập",
+    "uri": "/vi/index.html"
+  },
+  {
+    "breadcrumb": "Báo Cáo Thực Tập",
+    "content": "Week 1: Làm quen với Hugo và các dịch vụ cơ AWS cơ bản\nWeek 2: Doing task A…\nWeek 3: Doing task B…\nWeek 4: Doing task C…\nWeek 5: Doing task D…\nWeek 6: Doing task E…\nWeek 7: Doing task F…\nWeek 8: Doing task G…\nWeek 9: Doing task H…\nWeek 10: Doing task I…\nWeek 11: Doing task J…\nWeek 12: Doing task K…",
+    "description": "Week 1: Làm quen với Hugo và các dịch vụ cơ AWS cơ bản\nWeek 2: Doing task A…\nWeek 3: Doing task B…\nWeek 4: Doing task C…\nWeek 5: Doing task D…\nWeek 6: Doing task E…\nWeek 7: Doing task F…\nWeek 8: Doing task G…\nWeek 9: Doing task H…\nWeek 10: Doing task I…\nWeek 11: Doing task J…\nWeek 12: Doing task K…",
+    "tags": [],
+    "title": "Nhật ký công việc",
+    "uri": "/vi/1-worklog/index.html"
+  },
+  {
+    "breadcrumb": "Báo Cáo Thực Tập",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Thể loại",
+    "uri": "/vi/categories/index.html"
+  },
+  {
+    "breadcrumb": "Báo Cáo Thực Tập",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Thẻ",
+    "uri": "/vi/tags/index.html"
+  }
+]
